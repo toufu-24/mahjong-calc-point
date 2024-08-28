79757f9502dc7905f0bd6b475b8fbc2c90e3d182
toufu-24 <120612884+toufu-24@users.noreply.github.com>
2024-08-29 01:29:38 +0900

    add PyQt5

--- src/mahjong_calc_point/yolo/yolo_test.py
+++ src/mahjong_calc_point/yolo/yolo_test.py
@@ -1,15 +1,16 @@
 import torch
-from yolov5 import detect
+# from yolov5 import detect
+from yolov5 import detect as detect_yolo
 from pathlib import Path
 import cv2
 import matplotlib.pyplot as plt
 
 # モデルと画像のパス
 model_path = Path("./best.pt")
-image_path = Path("./img/watermark.jpg")
+image_path = Path("./img/dataset.jpg")
 
 # 推論の実行
-detect.run(
+detect_yolo.run(
     weights=str(model_path),
     source=str(image_path),
     imgsz=640,
@@ -28,8 +29,9 @@ detect.run(
 output_image_path = Path("runs/detect/exp/") / image_path.name
 
 # 画像の読み込みと表示
-img = cv2.imread(str(output_image_path))
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # OpenCVはBGR形式なのでRGBに変換
-plt.imshow(img)
-plt.axis("off")  # 軸を非表示
+# img = cv2.imread(str(output_image_path))
+# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# plt.show()
+img = cv2.imread("img/dataset.jpg")
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.show()
